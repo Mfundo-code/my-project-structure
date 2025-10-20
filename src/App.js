13fa4b7c5bf6b88f73e3157d2222c7e35a5d7d05
Mfundo <mfundoknox@gmail.com>
2025-10-20 10:30:13 +0200

    contacts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+// src/App.js
 import React from "react";
 import Header from "./GlobalComponents/Header";
 import Footer from "./GlobalComponents/Footer";
+
 import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Blog from "./pages/Blog/Blog";
+import Projects from "./pages/Projects/Projects";
+import Services from "./pages/Services/Services";
+import Contacts from "./pages/Contacts/Contacts";
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+
+      <main>
+        {/* All pages rendered sequentially for testing */}
+        <Home />
+        <About />
+        <Services />
+        <Projects />
+        <Blog />
+        <Contacts />
+      </main>
+
       <Footer />
     </>
   );
