c63e97d690e954fbe892d3fa77ddbccacc543c1b
Mfundo <mfundoknox@gmail.com>
2025-10-20 10:21:51 +0200

    app

---
+++ src/GlobalComponents/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <header>hey i'm Header</header>;
+};
+
+export default Header;
---
+++ src/pages/Home/HomeComponents/HeroSection.js
@@ -0,0 +1,3 @@
+import React from "react";
+const HeroSection = () => <section>hey i'm HeroSection</section>;
+export default HeroSection;
